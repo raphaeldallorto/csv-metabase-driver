160303f5ba93af778703497ebee7c890aa9b5623
raphaeldallorto <raphaeldallorto@hotmail.com>
2025-09-05 23:18:22 -0300

    fix: update plugin for Metabase v0.50 compatibility
    - Fixed missing honeysql/core classpath issue:
    
      Caused by: java.io.FileNotFoundException: Could not locate honeysql/core__init.class, honeysql/core.clj or honeysql/core.cljc on classpath.
    
    - Replaced/updated dependency to resolve:
    
      Could not find artifact br.com.markenson:csvjdbc:jar:1.3.1-metabase-SNAPSHOT in clojars (https://repo.clojars.org/)
    
    Ensures compatibility with Metabase 0.50.

--- csv/project.clj
+++ csv/project.clj
@@ -1,17 +1,21 @@
-(defproject metabase/csv-driver "1.0.1-SNAPSHOT"
+(defproject metabase/csv-driver "1.0.2-SNAPSHOT"
+  :description "CSV Driver for Metabase"
   :min-lein-version "2.5.0"
 
-  :repositories {"sonatype snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
-  :dependencies
-  [[br.com.markenson/csvjdbc "1.3.1-metabase-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [br.com.markenson/csvjdbc "1.3.1-metabase-SNAPSHOT" :local/root "lib/csvjdbc-1.3.1-metabase-SNAPSHOT.jar"]
+                 [honeysql "1.0.461"]
+                 [cheshire "5.11.0"]
+                 [clj-time "0.15.2"]
+                 [org.clojure/java.jdbc "0.7.12"]]
 
   :profiles
   {:provided
    {:dependencies [[metabase-core "1.0.0-SNAPSHOT"]]}
 
    :uberjar
-   {:auto-clean    true
-    :aot           :all
-    :javac-options ["-target" "1.8", "-source" "1.8"]
-    :target-path   "target/%s"
-    :uberjar-name  "csv.metabase-driver.jar"}})
+   {:auto-clean true
+    :aot :all
+    :target-path "target/%s"
+    :uberjar-name "csv.metabase-driver.jar"}})
+
--- csv/src/metabase/driver/csv.clj
+++ csv/src/metabase/driver/csv.clj
@@ -18,7 +18,7 @@
              [sync :as sql-jdbc.sync]]
             [metabase.driver.sql.query-processor :as sql.qp]
             [metabase.util
-             [date-2 :as du]
+             ;;[date_2 :as du]
              [honeysql-extensions :as hx]]
             [schema.core :as s])
   (:import [java.sql Time Timestamp]
@@ -49,14 +49,15 @@
 		(.setTime cal (.toDate date))
 		(.get cal Calendar/WEEK_OF_YEAR)))
 
-(defmethod sql.qp/date [:csv :day]            	[_ _ expr] (hsql/call :substring expr 1 10))
-(defmethod sql.qp/date [:csv :month]            [_ _ expr] (hsql/call :substring expr 1 7))
-(defmethod sql.qp/date [:csv :year]            	[_ _ expr] (hsql/call :year expr))
-(defmethod sql.qp/date [:csv :second]           [_ _ expr] (hsql/call :substring expr 1 10))
-(defmethod sql.qp/date [:csv :minute]           [_ _ expr] (hsql/call :substring expr 1 16))
-(defmethod sql.qp/date [:csv :hour]            	[_ _ expr] (hsql/call :substring expr 1 13))
-(defmethod sql.qp/date [:csv :hour-of-day]      [_ _ expr] (hsql/call :substring expr 1 13))
-(defmethod sql.qp/date [:csv :week]             [_ _ expr] (:week-of-year expr))
+;; Remova estes métodos obsoletos
+;;(defmethod sql.qp/date [:csv :day]            	[_ _ expr] (hsql/call :substring expr 1 10))
+;;(defmethod sql.qp/date [:csv :month]            [_ _ expr] (hsql/call :substring expr 1 7))
+;;(defmethod sql.qp/date [:csv :year]            	[_ _ expr] (hsql/call :year expr))
+;;(defmethod sql.qp/date [:csv :second]           [_ _ expr] (hsql/call :substring expr 1 10))
+;;(defmethod sql.qp/date [:csv :minute]           [_ _ expr] (hsql/call :substring expr 1 16))
+;;(defmethod sql.qp/date [:csv :hour]            	[_ _ expr] (hsql/call :substring expr 1 13))
+;;(defmethod sql.qp/date [:csv :hour-of-day]      [_ _ expr] (hsql/call :substring expr 1 13))
+;;(defmethod sql.qp/date [:csv :week]             [_ _ expr] (:week-of-year expr))
 
 (defmethod sql-jdbc.sync/database-type->base-type :csv [_ database-type]
   (database-type->base-type database-type))
@@ -64,18 +65,24 @@
 
 (defn is-http [path]  (if (clojure.string/starts-with? (clojure.string/lower-case path) "http") true false))
 
-(defmethod sql-jdbc.conn/connection-details->spec :csv [_ {:keys [csv separator charset advanced]
-                                                              :or   {csv "arquivo.csv"}
-                                                              :as   details}]
+(defmethod sql-jdbc.conn/connection-details->spec :csv
+  [_ {:keys [csv separator charset advanced]
+      :or {csv "arquivo.csv"}
+      :as details}]
 
-(def strHttp (if (is-http csv) ":class:br.markenson.com.csvjdbc4metabase.readers.HttpCSVReader" ""))
+  (let [strHttp (when (is-http csv)
+                  ":class:br.markenson.com.csvjdbc4metabase.readers.HttpCSVReader")
+        customBaseUrl (if (is-http csv)
+                        (str "&customBaseUrl=" csv)
+                        "")]
 
-(def customBaseUrl (if (is-http csv) (str "&customBaseUrl=" csv) ""))
+    (merge
+     {:classname   "org.relique.jdbc.csv.CsvDriver"
+      :subprotocol (str "relique:csv" strHttp)
+      :subname     (str (if (is-http csv) "" csv)
+                        "?separator=" separator
+                        "&charset=" charset
+                        customBaseUrl
+                        advanced)}
+     (dissoc details :csv :separator :charset :customBaseUrl :advanced))))
 
-(merge {:classname   "org.relique.jdbc.csv.CsvDriver"
-        :subprotocol (str "relique:csv" strHttp)
-        :subname     (str (if (is-http csv) "" csv) "?separator=" separator "&charset=" charset customBaseUrl advanced)
- }
-         (dissoc details :csv :separator :charset :customBaseUrl :advanced))  
-  
-)
